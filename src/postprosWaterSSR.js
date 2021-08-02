ebe50ac00fbbe13d227a6ca65f78ac89bbc3c254
Felix Mariotto <felixmariotto@gmail.com>
2021-08-02 12:40:48 +0200

    improve water reflection

--- src/postprosWaterSSR.js
+++ src/postprosWaterSSR.js
@@ -30,13 +30,21 @@ const shader = {
 
 			if ( length( waterValue.xyz ) > 0.5 ) {
 
-				vec3 depthColor = vec3( 0, 0, 0 );
+				// get scene reflected pixel
 				vec2 uv = vec2( vUv.x, ( 0.7 - vUv.y ) + waterValue.x * 0.25 );
 				vec3 reflectionColor = texture2D( tDiffuse, uv ).xyz;
 
-				float t = waterValue.y / ( waterValue.y + waterValue.z );
-				vec3 finalColor = mix( reflectionColor, depthColor, t );
-				sceneColor = vec4( finalColor, 1.0 );
+				// darker reflection with proximity
+				float waterFragT = 1.0 - ( vUv.y / 0.35 );
+				reflectionColor.x -= 0.6 * waterFragT;
+				reflectionColor.y -= 0.5 * waterFragT;
+				reflectionColor.z -= 0.55 * waterFragT;
+
+				// reflection power
+				float rp = ( waterValue.x * 2.0 + 0.5 ) * ( 0.3 + 0.7 * ( 1.0 - waterFragT ) );
+				reflectionColor = mix( vec3( 48.0/255.0, 43.0/255.0, 34.0/255.0 ), reflectionColor, rp );
+
+				sceneColor = vec4( reflectionColor, 1.0 );
 
 			}
 
